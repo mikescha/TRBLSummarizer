7db070f386f42fcf6d801275dcaaa0e40106f650
Michael Schackwitz <schack@gmail.com>
2022-08-08 14:54:54 +0100

    Cleaned up file checking and error handling

--- TRBL Summarizer/TRBLSummarizer.py
+++ TRBL Summarizer/TRBLSummarizer.py
@@ -1,5 +1,3 @@
-
-from this import d
 import streamlit as st
 import pandas as pd
 import numpy as np
@@ -16,6 +14,91 @@ import calendar
 # Constants and Globals
 #
 #
+data_foldername = 'Data'
+data_filename = data_foldername + '/' + 'data.csv'
+site_info_filename = data_foldername + '/' + 'sites.csv'
+weather_filename = data_foldername + '/' + 'weather_history.csv'
+
+bad_files = 'bad'
+sites = 'sites'
+filename_str = 'filename'
+site_str = 'site'
+malesong = 'malesong'
+altsong2 = 'altsong2'
+altsong1 = 'altsong1'
+courtsong = 'courtsong'
+date_str = 'date'
+hour_str = 'hour'
+tag_wse = 'tag_edge'
+tag_mhh = 'tag_fs'
+tag_wsm = 'tag_m'
+tag_mhe = 'tag_mc'
+tag_wsh = 'tag_wsh'
+tag_mhe2= 'tag_mhe2'
+tag_ws  = 'tag_ws'
+tag_mh  = 'tag_mh'
+tag_    = 'tag_'
+
+start_str = 'start'
+end_str = 'end'
+songs = [malesong, courtsong, altsong2, altsong1]
+tags = [tag_wse, tag_wsm, tag_mhe, tag_mhh, tag_wsh, tag_mhe2, tag_ws, tag_mh, tag_]
+
+columns = {data_filename : 'filename', 
+           site_str      : 'site', 
+           'day'         : 'day',
+           'month'       : 'month',
+           'year'        : 'year',
+           hour_str      : 'hour', 
+           date_str      : 'date',
+           tag_wse       : 'tag<reviewed-WS-e>',
+           tag_wsm       : 'tag<reviewed-WS-m>',
+           tag_mhe       : 'tag<reviewed-MH-e>',
+           tag_mhh       : 'tag<reviewed-MH-h>',
+           tag_wsh       : 'tag<reviewed-WS-h>',
+           tag_mhe2      : 'tag<reviewed-MH-e2>',
+           tag_ws        : 'tag<reviewed-WS>',
+           tag_mh        : 'tag<reviewed-MH>',
+           tag_          : 'tag<reviewed>',
+           malesong      : 'val<Agelaius tricolor/Common Song>',
+           altsong1      : 'val<Agelaius tricolor/Alternative Song>',
+           altsong2      : 'val<Agelaius tricolor/Alternative Song 2>',
+           courtsong     : 'val<Agelaius tricolor/Courtship Song>'}
+
+friendly_names = {malesong : 'Male', 
+                  courtsong: 'Chorus',
+                  altsong2 : 'Female', 
+                  altsong1 : 'Nestling',
+                  tag_wsm  : 'WS MM',
+                  tag_wse  : 'WS Edge',              
+                  tag_mhh  : 'MH MM',
+                  tag_mhe  : 'MH Edge',
+                  tag_wsh  : 'WS-h',
+                  tag_mhe2 : 'MH-e2',
+                  tag_ws   : 'WS',
+                  tag_mh   : 'MH',
+                  tag_     : 'WS orig'}
+
+
+#Make the map of column names to friendly names
+col_map = {}
+for song in songs:
+    col_map[columns[song]] = friendly_names[song]
+
+for song in songs:
+    col_map[columns[song]] = friendly_names[song]
+
+#Make the map of column names to friendly names
+col_tag_map = {}
+for tag in tags:
+    col_tag_map[columns[tag]] = friendly_names[tag]
+
+#
+#Graphing-related constants
+#
+tick_spacing = 14
+
+
 data_foldername = 'Data/'
 data_filename = 'data.csv'
 site_info_filename = 'sites.csv'
@@ -27,7 +110,7 @@ file_types = ["Young Nestling", "Mid Nestling", "Old Nestling", "Female", "Male"
 #
 #
 def show_error(msg: str):
-    st.error(msg + " This may not work correctly.")
+    st.error("Whoops! " + msg + "! This may not work correctly.")
 
 
 #
@@ -35,11 +118,20 @@ def show_error(msg: str):
 #File handling and setup
 #
 #
+def site_name(file_name:str, sites:list) -> str:
+    site = ''
+    for s in sites:
+        if file_name[0:len(s)] == s:
+            site = s
+            break
+    return site
+
 def get_target_sites() -> dict:
     file_summary = {}
     for t in file_types:
         file_summary[t] = []
-    file_summary['bad'] = []
+    file_summary[bad_files] = []
+    file_summary[sites] = set()
 
     #Load the list of unique site names, keep just the 'Name' column, and then convert that to a list
     data_dir = Path(__file__).parents[0] / data_foldername
@@ -57,20 +149,33 @@ def get_target_sites() -> dict:
         found = False
         if f[-4:] == '.csv':  #must be CSV
             for s in site_list:
-                if s.lower() == f[0:len(s)].lower(): # If the first part of the filename matches a site
+                if s.lower() == f[0:len(s)].lower() and not found: # If the first part of the filename matches a site
                     f_type = f[len(s)+1:len(f)] # Cut off the site name
                     for t in file_types:
-                        #CONVERT both to lower case before comparing
                         if t.lower() == f_type[0:len(t)].lower():
                             file_summary[t].append(f)
+                            file_summary[sites].add(s)
                             found = True
+                            break
         if not found:
             if f != data_filename and f != site_info_filename: 
-                file_summary['bad'].append(f)
-
-
+                file_summary[bad_files].append(f)
+    
+    #Confirm that there are the same set of files for each type
+    if len(file_summary[sites]) > 0:
+        for t in file_types:
+            if len(file_summary[sites]) != len(file_summary[t]):
+                if len(file_summary[t]) == 0:
+                    show_error('Missing all files of type ' + t)
+                else:
+                    show_error('Wrong number of files of type ' + t)
+    else:
+        show_error('No site files found')
 
     return file_summary
+
+def get_site_to_analyze(site_list:list) -> str:
+    return st.sidebar.selectbox('Site to summarize', site_list)
 
 #
 #
@@ -80,5 +185,15 @@ def get_target_sites() -> dict:
 st.title('TRBL Summary')
 
 #Get the list of sites that we're going to do reports for
+site_list = get_target_sites()
+site = get_site_to_analyze(site_list[sites])
+st.subheader(site)
 
-get_target_sites()
+
+
+
+
+if len(site_list[bad_files]) == 0:
+    with st.expander("See possibly bad filenames"):  
+        st.write(site_list[bad_files])
+
