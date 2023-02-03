02b83f8dc9d4caaae050bac2e0941e0f8fe66dd3
Michael Schackwitz <schack@gmail.com>
2023-02-02 18:37:24 -0600

    Added legend and fixed minor bugs

--- TRBLSummarizer/test.py
+++ TRBLSummarizer/test.py
@@ -1,25 +1,57 @@
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
-def plot(data, w, h):
-    dpi = 300
+w = 6.5
+h = 3
 
-    fig, ax = plt.subplots(figsize = (w,h), dpi=dpi)
-    ax.plot(data)
-    fname = "file_{}.png".format(w)
+def make_plot(df, dpi):
+    cmap = ['Reds', 'Blues', 'Greens']
+    row_count = len(cmap)
+    fig, axs = plt.subplots(nrows = row_count, ncols = 1,
+                            sharex = 'col', dpi=dpi,
+                            gridspec_kw={'height_ratios': np.repeat(1,row_count), 
+                                            'left':0, 'right':1, 'bottom':0, 'top':1,
+                                            'hspace':0},
+                            figsize=(w,h))
+    r=0
+    width = len(df.columns) * (dpi/100)
+    for c in cmap:
+        axs[r].imshow(df, cmap=c, interpolation='nearest',
+                    origin='lower', extent=(0,width,0,width/w))
+        axs[r].title.set_visible(False)
+        axs[r].axis('off')
+        r += 1
+    
+    fname = f"plot_{dpi}dpi.png"
     plt.savefig(fname)
+    plt.show()
     plt.close()
 
+dates = pd.date_range("2022-01-01", "2022-01-30").tolist()
+temp_data = np.arange(0,30)
+df = pd.DataFrame(data=temp_data, index=dates, columns=["High temp"])
+df = df.transpose()
 
-dates = pd.date_range("2022-01-01", "2022-01-06").tolist()
-temp_data = [60,70,80,70,60,80]
-precip_data = [0,0,1,0.5,0,.6]
+make_plot(df, 100)
+make_plot(df, 300)
 
-df_temp = pd.DataFrame(data=temp_data, index=dates, columns=["High temp"])
-df_precip = pd.DataFrame(data=precip_data, index=dates, columns=["Precip"])
 
-w = 10
-h = 5
 
-plot(df_temp, w, h)
-plot(df_precip, w + 1, h)
+#        x = np.arange(30)
+#        y = 3.*np.sin(x*2.*np.pi/30.)
+#        #axs[r].plot(x, y)
+
+#fig.tight_layout()
+#plt.subplots_adjust(left=0, bottom=0, right=0.001, top=0.001, wspace=0, hspace=0)
+
+#axs[r].autoscale(enable=False, axis='both')
+#axs[r].set_xlim(0,100*scale)
+#axs[r].set_ylim(0,10*scale)
+
+#axs[r].tick_params(
+#    axis='y',
+#    which='both',      # both major and minor ticks are affected
+##    left=False, right=False,  # ticks along the sides are off
+#   labelleft=False, labelright=False) # labels on the Y are off 
+#axs[r].margins(x=0,y=0)
