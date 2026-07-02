c1b26d2874c4594fec8107a19d4f63f95a84f524
Michael Schackwitz <22506792+mikescha@users.noreply.github.com>
2026-07-02 09:13:55 -0700

    streamlit flag

--- TRBLSummarizer/trbl_summarizer.py
+++ TRBLSummarizer/trbl_summarizer.py
@@ -45,7 +45,7 @@ from matplotlib.transforms import Bbox
 from PIL import Image, ImageDraw, ImageFont
 
 # Set appropriately before I deploy
-BEING_DEPLOYED_TO_STREAMLIT = False
+BEING_DEPLOYED_TO_STREAMLIT = True
 # Dec 2025, we may or may not want to show the manual analysis graph
 SHOW_MANUAL_ANALYSIS = True
 
